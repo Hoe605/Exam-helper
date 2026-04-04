06c49f3dc21c1d81426b61216fe0b5961544f909
Hoe <1376929560@qq.com>
2026-04-04 18:18:00 +0800

    1

--- start_llm.bat
+++ start_llm.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting Local LLM Server...
-"D:\Development\llama-b8661-bin-win-cuda-12.4-x64\llama-server.exe" -m "D:\Code\llm\gemma-4-gguf\llm\gemma.gguf" -ngl 99 --flash-attn on -c 2048
+"D:\Development\llama-b8661-bin-win-cuda-12.4-x64\llama-server.exe" -m "D:\Code\llm\gemma-4-gguf\llm\gemma.gguf" -ngl 99 --flash-attn on -c 4096
 pause
